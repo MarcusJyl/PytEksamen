fd78f60861078ca81954b0fb3f72e783680a7cf3
Marc Ekstrom <marc.g.ekstrom@gmail.com>
2021-05-25 09:57:44 +0200

    odel change

---
+++ modules/model.py
@@ -0,0 +1,74 @@
+import sys
+from matplotlib import pyplot
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.models import load_model
+import h5py
+import numpy as np
+
+
+
+# define cnn model 
+def define_model(output_size):  
+	model = Sequential()
+    #try 224, 248, 496
+	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(128, 128, 3)))
+	model.add(BatchNormalization())
+	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(BatchNormalization())
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Dropout(0.2))
+	model.add(Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(BatchNormalization())
+	model.add(Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(BatchNormalization())
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Dropout(0.3))
+	model.add(Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(BatchNormalization())
+	model.add(Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(BatchNormalization())
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Dropout(0.4))
+	model.add(Flatten())
+	model.add(Dense(512, activation='relu', kernel_initializer='he_uniform'))
+	model.add(BatchNormalization())
+	model.add(Dropout(0.5))
+	model.add(Dense(output_size, activation='softmax'))
+	# compile model
+	opt = SGD(lr=0.001, momentum=0.9)
+	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+	return model
+
+
+
+
+
+# run the test harness for evaluating a model
+def run_test_harness(images, targets, epochs, output_size):
+# define model
+	model = define_model(output_size)
+	# model = load_model('final_model.h5')
+	# create data generator
+	datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
+#     featurewise_center=True
+	# specify imagenet mean values for centering
+# 	datagen.mean = [123.68, 116.779, 103.939]
+	# prepare iterator
+	train_it = datagen.flow(images, targets)
+	# fit model
+	model.fit_generator(train_it, steps_per_epoch=len(train_it), epochs=epochs, verbose=1)
+	# save model
+	model.save('final_model.h5')
+ 
+# # entry point, run the test harness
+# run_test_harness()
